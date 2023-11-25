539072f62cb1fcb2baed65a9e6d7d8dcf3ae7011
Artjom Gussev <artjomgussev2001@gmail.com>
2023-11-25 22:43:52 +0200

    add type to context

--- src/components/LocaleContextProvider/LocaleContextProvider.tsx
+++ src/components/LocaleContextProvider/LocaleContextProvider.tsx
@@ -1,9 +1,17 @@
 'use client';
-import React, { ReactNode, useContext, useState } from 'react';
-const LocaleContext = React.createContext({});
+import React, { ReactNode, useContext, useState, Dispatch, SetStateAction } from 'react';
+interface LocaleContextState {
+  currentLocale: string;
+  setCurrentLocale: Dispatch<SetStateAction<string>>;
+}
+
+const LocaleContext = React.createContext<LocaleContextState>({
+  currentLocale: 'et', // Default value
+  setCurrentLocale: () => {}, // Placeholder function
+});
 
 const LocaleContextProvider = ({ children }: { children: ReactNode }) => {
-  const [currentLocale, setCurrentLocale] = useState('et');
+  const [currentLocale, setCurrentLocale] = useState<string>('et');
 
   return <LocaleContext.Provider value={{ currentLocale, setCurrentLocale }}>{children}</LocaleContext.Provider>;
 };
